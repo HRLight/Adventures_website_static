961f3cf0df3314975600902ec9f3564291bcfd17
HRLight <91833561+HRLight@users.noreply.github.com>
2022-11-15 18:35:58 +0800

    updated

--- dist/assets/js/menu.js
+++ dist/assets/js/menu.js
@@ -1,34 +1,34 @@
 //show and Hide menu Items
 
-    function home1(){
+        function home1(){
 
 
-            console.log("home1");
+                console.log("home1");
 
-            var home_navbar = document.getElementById("home_navbar");
-            var home = document.getElementById("home");
-            var home_menu = document.getElementById("home_menu");
-            var home_des = document.getElementById("home_des");
-            var home_show =  document.getElementById("home_show").checked ;
+                var home_navbar = document.getElementById("home_navbar");
+                var home = document.getElementById("home");
+                var home_menu = document.getElementById("home_menu");
+                var home_des = document.getElementById("home_des");
+                var home_show =  document.getElementById("home_show").checked ;
 
-            home_show == true ? home_navbar.classList.remove("hidden") : home_navbar.classList.add("hidden");
-            home_show == true ? home.classList.remove("hidden") : home.classList.add("hidden");
-            home_show == true ? home_menu.classList.remove("hidden") : home_menu.classList.add("hidden");
-            home_show == true ? home_des.classList.remove("hidden") : home_des.classList.add("hidden");
+                home_show == true ? home_navbar.classList.remove("hidden") : home_navbar.classList.add("hidden");
+                home_show == true ? home.classList.remove("hidden") : home.classList.add("hidden");
+                home_show == true ? home_menu.classList.remove("hidden") : home_menu.classList.add("hidden");
+                home_show == true ? home_des.classList.remove("hidden") : home_des.classList.add("hidden");
 
-            console.log(home_show, "home_show");  
-     }
+                console.log(home_show, "home_show");  
+        }  
 
-    function about1(){
-            var about_navbar = document.getElementById("about_navbar");
-            var about = document.getElementById("about");
-            var about_edit = document.getElementById("about_edit_menu");
-            var about_show=document.getElementById("about_show").checked;
-            about_show == true ?  about_navbar.classList.remove("hidden") : about_navbar.classList.add("hidden");
-            about_show == true ?  about.classList.remove("hidden") :    about.classList.add("hidden");
-            about_show == true ?  about_edit.classList.remove("hidden") :   about_edit.classList.add("hidden");
-            console.log(about_show, "about_show");
-    }
+        function about1(){
+                var about_navbar = document.getElementById("about_navbar");
+                var about = document.getElementById("about");
+                var about_edit = document.getElementById("about_edit_menu");
+                var about_show=document.getElementById("about_show").checked;
+                about_show == true ?  about_navbar.classList.remove("hidden") : about_navbar.classList.add("hidden");
+                about_show == true ?  about.classList.remove("hidden") :    about.classList.add("hidden");
+                about_show == true ?  about_edit.classList.remove("hidden") :   about_edit.classList.add("hidden");
+                console.log(about_show, "about_show");
+        }
 
 
         function service(){
@@ -43,7 +43,22 @@
             service_show == true ?  service_edit.classList.remove("hidden") :   service_edit.classList.add("hidden");
          }
 
-    // Auto display In Text of menu items 
+
+
+         function portfolio(){
+
+            var porfolio_navbar = document.getElementById("porfolio_navbar");
+            var portfolio = document.getElementById("portfolio");
+            var portfolio_edit = document.getElementById("portfolio_edit");
+            var portfolio_show =document.getElementById("portfolio_show").checked;
+
+            portfolio_show == true ?  porfolio_navbar.classList.remove("hidden") : porfolio_navbar.classList.add("hidden");
+            portfolio_show == true ?  portfolio.classList.remove("hidden") :    portfolio.classList.add("hidden");
+            portfolio_show == true ?  portfolio_edit.classList.remove("hidden") :   portfolio_edit.classList.add("hidden");
+         }
+
+        
+        // Auto display In Text of menu items 
         function home_display()
         {                    
             let discription=document.getElementById("title1").value;
@@ -94,9 +109,9 @@
                     ContestantNum = 1,
                     i;
                     for(i=0; i<ContestantNum; i++){
-                        var clone = service.cloneNode(true);
-                        form.appendChild(clone);
-                        service.classList.remove("hidden");
+                    var clone = service.cloneNode(true);
+                    form.appendChild(clone);
+                    service.classList.remove("hidden");
 
                     let add_service_title=document.getElementById("add_service_title").value;
                     document.getElementById("display_service_title").innerText=add_service_title; 
@@ -104,14 +119,14 @@
                     let add_service_description=document.getElementById("add_service_description").value;
                     document.getElementById("display_service_description").innerText=add_service_description; 
                     }
-                        
-                    }                                   
+                                
+            }                                   
 
                     function display_stategy(){
 
                         let strategy=document.getElementById("add_strategy").value;
                         document.getElementById("strategy").innerText=strategy; 
-                        
+                                    
                     }
 
 
@@ -128,22 +143,21 @@
                         document.getElementById("launch").innerText=strategy; 
                         
                     }
-
+                    // Pending Cannot add portfolio
                     function add_portfolio(){
                         alert("Are you sure you want to add this service");
-                        var portfolio = document.getElementById("add_portfolio"),
-                        formd = document.getElementsById("form_oo")[0],
+                        var portfolio = document.getElementById("add_portfolio");
+                        let portfolio2 = document.getElementsById("portfolio2")[0],
                         ContestantNum = 1,
                         i;
-
                         for(i=0; i<ContestantNum; i++){
                             var clone = portfolio.cloneNode(true);
-                            formd.appendChild(clone);
+                            portfolio2.appendChild(clone);
                             portfolio.classList.remove("hidden");
     
                             // let add_service_title=document.getElementById("add_service_title").value;
                             // document.getElementById("display_service_title").innerText=add_service_title; 
-    
+                            
                             // let add_service_description=document.getElementById("add_service_description").value;
                             // document.getElementById("display_service_description").innerText=add_service_description; 
                         }
