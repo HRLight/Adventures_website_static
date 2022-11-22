4c41d1f531ba25f0f914e54beadf76d126d637d2
HRLight <91833561+HRLight@users.noreply.github.com>
2022-11-22 10:58:38 +0800

    updated

--- dist/assets/js/menu.js
+++ dist/assets/js/menu.js
@@ -27,7 +27,6 @@
         }
 
         function service(){
-
             var service_navbar = document.getElementById("service_navbar");
             var service = document.getElementById("features");
             var service_edit = document.getElementById("service_edit");
@@ -41,7 +40,6 @@
 
 
          function portfolio(){
-
             var porfolio_navbar = document.getElementById("porfolio_navbar");
             var portfolio = document.getElementById("portfolio");
             var portfolio_edit = document.getElementById("portfolio_edit");
@@ -61,43 +59,52 @@
             if(home_title==null || home_title==""){
                 home_title="Sample Title";
             }
-            
-            document.getElementById("output1").innerText=home_title;
-            //$('#output1').text(new_business_name);
-            localStorage.setItem("Home Title", home_title);         
-
-            
+    
+            localStorage.setItem("Home_title", home_title);    
+            var Home_Title = localStorage.getItem("Home_title");
+            document.getElementById("output1").innerText=Home_Title;
             let home_discription=document.getElementById("add_home_description").value;
 
             if(home_discription==null || home_discription==""){
                 home_discription="Sample Description"
 
             }
-            document.getElementById("home_des").innerText=home_discription; 
-            localStorage.setItem("Home Description", home_discription);
+            localStorage.setItem("Home_Description", home_discription);
+            var home_discription_store=localStorage.getItem("Home_Description");
+            document.getElementById("home_des").innerText=home_discription_store; 
+
         }
 
+
+
+        //About Company Start of editable display
         function about_display_txt()
         {
             let add_service_title=document.getElementById("add_service_title").value;
-            document.getElementById("about-txt1").innerText=add_service_title;
+            localStorage.setItem("About", add_service_title);
+            
+            if(add_service_title==null || add_service_title==""){
+               add_service_title ="About US";
+
+            }
+           
+            var About= localStorage.getItem("About");
+            document.getElementById("about-txt1").innerText=About;
+
+
+
 
             let title3=document.getElementById("add_description_about").value;
-            document.getElementById("about-txt3").innerText=title3;
-        }
+            localStorage.setItem("Company_Story", title3);
+            if(title3==null || title3==""){
+                title3= "comapany story";
+            }
+            var company_story=localStorage.getItem("Company_Story");
+            document.getElementById("about-txt3").innerText=company_story; 
+        }//end of about display
+        
 
-        function about_text_display()
-        {
-            let title1=document.getElementById("title1").value;
-            document.getElementById("about-txt1").innerText=title1;
-
-            let title2=document.getElementById("title2").value;
-            document.getElementById("about-txt3").innerText=title2;
-
-            let title3=document.getElementById("title3").value;
-            document.getElementById("about-txt3").innerText=title3;
-        }
-
+        // start of the Background nav Color 
         function updateTopInfoBGColorClick(){
             var bgColorValue = document.getElementById("top_info_bg_color_selected");
             var TOP_INFO_DIV = document.getElementById("navbar");
@@ -107,9 +114,13 @@
             console.log(TOP_INFO_DIV.className);
 
             localStorage.setItem("navbar" , "navbar bg-["+bgColorValue.value+"]");    
-        }
+        }//end of the nav Color
 
-            function add_services(){
+
+
+
+        // Start of Service module 
+        function add_services(){
                     alert("Are you sure you want to add service");
                     var service = document.getElementById("add_service"),
                     form = document.getElementsByTagName("form")[0],
@@ -121,24 +132,30 @@
                     service.classList.remove("hidden");
 
                     let add_service_title=document.getElementById("add_service_title").value;
-                    document.getElementById("display_service_title").innerText=add_service_title; 
+                     localStorage.setItem("Service" , add_service_title);
+                     if(add_service_title==null || add_service_title==""){
+                        add_service_title="None"
+
+                     }
+                     var Service_title=localStorage.getItem("Service");
+                     document.getElementById("display_service_title").innerText=Service_title;
+
+                    // document.getElementById("display_service_title").innerText=add_service_title; 
 
                     let add_service_description=document.getElementById("add_service_description").value;
                     document.getElementById("display_service_description").innerText=add_service_description; 
-                    }
+                }
                                 
-            }                                   
+        }//end of Service                              
 
-                    function display_stategy(){
+                function display_stategy(){
 
                         let strategy=document.getElementById("add_strategy").value;
                         document.getElementById("strategy").innerText=strategy; 
                                     
-                    }
-
+                }
 
                     function display_development(){
-
                         let strategy=document.getElementById("add_development").value;
                         document.getElementById("development").innerText=strategy; 
                         
@@ -150,6 +167,12 @@
                         document.getElementById("launch").innerText=strategy; 
                         
                     }
+
+
+
+
+
+
                     // Pending Cannot add portfolio
                     function add_portfolio(){
                         alert("Are you sure you want to add this service");
@@ -169,3 +192,6 @@
                             // document.getElementById("display_service_description").innerText=add_service_description; 
                         }
                     }
+
+            
+              
